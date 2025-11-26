7d560c2e01f3eafb36437af823a5c6049a856cf7
Sebabebab <smedsrudsebastian@gmail.com>
2025-11-26 16:20:19 +0100

    Kjør appen på port 8080
    Co-authored-by: Jonas Nicolaysen <jonas.nicolaysen@nav.no>

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,10 +4,12 @@ import com.example.config.ApplicationState
 import com.example.config.commonConfig
 import com.example.config.internalNaisRoutes
 import io.ktor.server.application.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import io.ktor.server.routing.routing
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
 }
 
 fun Application.module() {
