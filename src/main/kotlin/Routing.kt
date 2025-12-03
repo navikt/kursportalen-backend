91121762e2075bf193b738d9af7169b8d1f54625
Sebabebab <smedsrudsebastian@gmail.com>
2025-12-03 14:59:42 +0100

    Legg til tilgangspolicy for utgående regler i naiserator-dev.yaml

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,11 +1,21 @@
 package com.example
 
 import com.example.service.BtcPriceService
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BtcPriceResponse(val price: String)
 
 fun Application.configureRouting() {
+    install(ContentNegotiation) {
+        json()
+    }
+
     val btcPriceService = BtcPriceService()
 
     routing {
@@ -14,7 +24,7 @@ fun Application.configureRouting() {
         }
         get("/bitcoinprice") {
             val price = btcPriceService.getBtcPrice()
-            call.respondText(price)
+            call.respond(BtcPriceResponse(price = price))
         }
     }
 }
