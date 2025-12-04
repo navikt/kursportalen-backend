69c63ba4f35ee70db0066a4c10dc2c9fab99bdee
Sebabebab <smedsrudsebastian@gmail.com>
2025-12-04 09:51:39 +0100

    går tilbake til gamle filer hvor ptosjektet deployet

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,21 +1,11 @@
 package com.example
 
 import com.example.service.BtcPriceService
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class BtcPriceResponse(val price: String)
 
 fun Application.configureRouting() {
-    install(ContentNegotiation) {
-        json()
-    }
-
     val btcPriceService = BtcPriceService()
 
     routing {
@@ -24,7 +14,7 @@ fun Application.configureRouting() {
         }
         get("/bitcoinprice") {
             val price = btcPriceService.getBtcPrice()
-            call.respond(BtcPriceResponse(price = price))
+            call.respondText(price)
         }
     }
 }
