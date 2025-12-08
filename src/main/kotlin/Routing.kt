312a961401138ce90e1d66bf62328b44a6d66fe7
Sebabebab <smedsrudsebastian@gmail.com>
2025-12-08 12:21:59 +0100

    Går tilbake til gamle filer som deployet

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -9,9 +9,12 @@ fun Application.configureRouting() {
     val btcPriceService = BtcPriceService()
 
     routing {
+        get("/") {
+            call.respondText("Velkommen til Kursportalen!")
+        }
         get("/bitcoinprice") {
             val price = btcPriceService.getBtcPrice()
-            call.respond(mapOf("price" to price))
+            call.respondText(price)
         }
     }
 }
