b3ec89ec05c0077794f2ff6684adbc015cb5ff18
Sebabebab <smedsrudsebastian@gmail.com>
2025-12-08 12:31:27 +0100

    Legg til støtte for innholdsforhandling og endre responsformat for bitcoinpris

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,9 +3,11 @@ package com.example
 import com.example.config.ApplicationState
 import com.example.config.commonConfig
 import com.example.config.internalNaisRoutes
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.routing
 
 fun main() {
@@ -16,6 +18,11 @@ fun Application.module() {
     val applicationState = ApplicationState()
 
     commonConfig()
+
+    install(ContentNegotiation) {
+        json()
+    }
+
     routing {
         internalNaisRoutes(applicationState)
         configureRouting()
