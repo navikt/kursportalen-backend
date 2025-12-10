f0914f0fdc2859a090a486cde7b03ec51f322ddc
Sebabebab <smedsrudsebastian@gmail.com>
2025-12-10 11:41:01 +0100

    Legg til Ktor CORS-modul i build.gradle.kts og oppdater libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,10 @@ dependencies {
     implementation(libs.ktor.client.apache)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.cors)
     implementation(libs.logback.classic)
-    implementation("io.ktor:ktor-server-cors-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
-
 }
+
